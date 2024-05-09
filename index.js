5653ed1ea83cf79e51e4b6957036a44cce23c2f9
imranhasan2 <imranhasan030303@gmail.com>
2024-05-09 23:35:16 +0600

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express()
 require('dotenv').config()
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const cors = require('cors')
@@ -8,8 +11,13 @@ const cors = require('cors')
 
 // middleWare
 
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+}))
 app.use(express.json())
+app.use(cookieParser())
+
 
 
 
@@ -27,6 +35,31 @@ const client = new MongoClient(uri, {
     }
 });
 
+
+
+
+const verifyToken = async (req, res, next) => {
+    const token = req.cookies?.token;
+
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized response' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+
+        if (err){
+            res.status(401).send({message : 'unauthorized token'})
+        }
+        req.user = decoded;
+        next()
+
+    })
+
+
+
+}
+
+
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -36,34 +69,113 @@ async function run() {
 
         const servicesCollection = client.db('carDoctor').collection('services')
 
+        //token auth
+        app.post('/jwt', async (req, res) => {
+            const user = req.body
+            console.log(user)
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1hr' })
+
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: false,
+
+
+                })
+                .send({ success: true })
+
+        })
+
+
+
+        //service related api
+
         const ordersCollection = client.db('carDoctor').collection('orders')
 
-        app.get('/services',async(req,res) =>{
+        app.get('/services', async (req, res) => {
             const cursor = servicesCollection.find()
             const result = await cursor.toArray()
             res.send(result)
         })
 
-        app.get('/services/:id',async(req,res) =>{
+        app.get('/services/:id', async (req, res) => {
             const id = req.params.id
-            const query ={_id : new ObjectId (id)}
+            const query = { _id: new ObjectId(id) }
             const options = {
-               
-                projection: {  title: 1, price: 1,service_id :1, img: 1,
+
+                projection: {
+                    title: 1, price: 1, _id: 1, img: 1,
+
                 },
-              };
-            const result = await servicesCollection.findOne(query,options)
+            };
+            const result = await servicesCollection.findOne(query, options)
 
             res.send(result)
         })
 
-        app.post('/orders', async( req,res) =>{
+        app.post('/orders', async (req, res) => {
             const order = req.body;
             console.log(order)
 
+
             const result = await ordersCollection.insertOne(order)
             res.send(result)
-            
+
+        })
+
+
+
+        app.get('/orders',verifyToken, async (req, res) => {
+            console.log(req.query.email)
+
+           // console.log('token', req.cookies.token)
+
+           console.log('user in valid token', req.user)
+
+           if(req.query.email !== req.user.email){
+            return res.status(403).send({message : 'Forbidden Access'})
+           }
+
+            let query = {}
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+            const result = await ordersCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.get('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: new ObjectId(id) }
+            const result = await ordersCollection.findOne(query);
+            res.send(result)
+        })
+
+
+
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: new ObjectId(id) }
+            console.log(query)
+            const result = await ordersCollection.deleteOne(query);
+
+            res.send(result)
+        })
+
+        app.patch('/orders/:id', async (req, res) => {
+            const id = req.params.id
+            const update = req.body
+            console.log(update)
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    status: update.status
+                },
+            };
+            const result = await ordersCollection.updateOne(filter, updateDoc)
+            res.send(result)
         })
 
 
